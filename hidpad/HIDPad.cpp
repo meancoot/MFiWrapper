62b460b4e7dd1f79a7a8ff9148b139a6572394cf
meancoot <jasonfetters@gmail.com>
2014-01-28 19:38:55 -0500

    Add license headers.

--- hidpad/HIDPad.cpp
+++ hidpad/HIDPad.cpp
@@ -1,3 +1,18 @@
+/*  MFiWrapper
+ *  Copyright (C) 2014 - Jason Fetters
+ * 
+ *  MFiWrapper is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  MFiWrapper is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with MFiWrapper.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdint.h>
 #include <string.h>
 
